5ad4a911e28f42765e3873e4083dd8bf5d1d3cbe
=princevora <=princevora522@gmail.com>
2024-06-08 15:19:00 +0530

    Poll bug fix

--- resources/views/pages/poll-results.blade.php
+++ resources/views/pages/poll-results.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Poll Result - {{request()->poll_id}}</title>
+    <title>Poll Result - {{ request()->poll_id }}</title>
     <style>
         .highcharts-credits {
             display: none;
@@ -27,30 +27,31 @@
                             </a>
                             <div class="col-xl-8 col-md-12">
                                 <div class="card card-plain">
-                                    <div class="card-body border-top border-5 border-info shadow-lg rounded text-center">
+                                    <div
+                                        class="card-body border-top border-5 border-info shadow-lg rounded text-center">
                                         <div class="d-flex justify-content-start">
                                             <span class="text-bold">
                                                 @php
                                                     $pollData = $fieldData->first();
                                                     $pollName = $pollData->poll_name;
                                                 @endphp
-                                                
+
                                                 <!-- Poll Name -->
-                                                {{$pollName}}
+                                                {{ $pollName }}
 
                                             </span> &nbsp;
-                                                <!-- SHow Who created The poll -->
-                                                by a {{$pollData->created_by == 1 ? 'user' : 'guest'}} · <b>
+                                            <!-- SHow Who created The poll -->
+                                            by a {{ $pollData->created_by == 1 ? 'user' : 'guest' }} · <b>
 
                                                 <!-- SHow when the poll was created -->
-                                                &nbsp; {{$pollData->created_at->diffForHumans()}}
+                                                &nbsp; {{ $pollData->created_at->diffForHumans() }}
                                             </b>
                                         </div>
                                         <div class="row">
                                             <!-- Form column -->
                                             <div class="col-md-6 mt-4">
                                                 @foreach ($fieldData as $data)
-                                                <!-- Declare A Var And Save Json To An Array -->
+                                                    <!-- Declare A Var And Save Json To An Array -->
 
                                                     @php
                                                         $poll_id = request()->poll_id;
@@ -65,38 +66,45 @@
 
                                                             <!-- Save Total Votes For the field in A Var -->
                                                             @php
-                                                                $vote = @$chartData->where('vote_field','=', $key)->first()->votes;
-                                                                $percentage = number_format($vote / $totalVotes * 100);
-                                                                $bgClass = ($prePer !== null && $percentage > $prePer) ? 'bg-success' : '';
+                                                                $vote = @$chartData
+                                                                    ->where('vote_field', '=', $key)
+                                                                    ->first()->votes;
+                                                                $percentage = number_format(
+                                                                    ($vote / $totalVotes) * 100,
+                                                                );
+                                                                $bgClass =
+                                                                    $prePer !== null && $percentage > $prePer
+                                                                        ? 'bg-success'
+                                                                        : '';
                                                                 $prePer = $percentage;
                                                             @endphp
 
                                                             <div class="row">
                                                                 <div class="d-flex justify-content-start">
-                                                                    <label for="" class="form-label">{{$field}}</label>
+                                                                    <label for=""
+                                                                        class="form-label">{{ $field }}</label>
                                                                 </div>
                                                                 <div class="d-flex justify-content-end">
-                                                                    <label for="" class="form-label">{{ $percentage }} % ({{$vote}} votes) </label>
+                                                                    <label for=""
+                                                                        class="form-label">{{ $percentage }} %
+                                                                        ({{ $vote }} votes) </label>
                                                                 </div>
                                                             </div>
 
                                                             <div class="progress" style="height: 18px">
-                                                                <div 
-                                                                    class="progress-bar h-auto {{$bgClass}}" 
-                                                                    role="progressbar" 
-                                                                    style="width: {{$percentage}}%;" 
-                                                                    aria-valuenow="{{$percentage}}" 
-                                                                    aria-valuemin="0" 
-                                                                    aria-valuemax="100"
-                                                                ></div>
+                                                                <div class="progress-bar h-auto {{ $bgClass }}"
+                                                                    role="progressbar"
+                                                                    style="width: {{ $percentage }}%;"
+                                                                    aria-valuenow="{{ $percentage }}"
+                                                                    aria-valuemin="0" aria-valuemax="100"></div>
                                                             </div>
-                                                        </div>                                                       
+                                                        </div>
                                                     @endforeach
                                                 @endforeach
 
                                                 <hr>
 
-                                                <p>Total votes: <b>{{$totalVotes}}</b></p>
+                                                <p>Total votes: <b>{{ $totalVotes }}</b></p>
 
                                                 <!-- Total votes -->
                                             </div>
@@ -106,45 +114,40 @@
                                                 <div id="container_chart"></div>
                                             </div>
                                         </div>
-                                       <div class="row">
-                                        <div class="col-sm-6">
-                                            <div class="input-group mb-3">
-                                                <input type="text" value="{{request()->poll_id}}" id="poll_id" style="display: none">
-                                                <input type="text" class="form-control texssd" id="window_url">
-                                                <div class="input-group-append"> 
-                                                  <button 
-                                                        onclick="sharePoll()" 
-                                                        class="btn-dark-outline input-group-text text-dark" 
-                                                        id="basic-addon2"
-                                                        title="Share to anyone" 
-                                                        data-toggle="tooltip"
-                                                    >
-                                                    <i class="fa-solid fa-share"></i> 
-                                                </button>
+                                        <div class="row">
+                                            <div class="col-sm-6">
+                                                <div class="input-group mb-3">
+                                                    <input type="text" value="{{ request()->poll_id }}"
+                                                        id="poll_id" style="display: none">
+                                                    <input type="text" class="form-control texssd" id="window_url">
+                                                    <div class="input-group-append">
+                                                        <button onclick="sharePoll()"
+                                                            class="btn-dark-outline input-group-text text-dark"
+                                                            id="basic-addon2" title="Share to anyone"
+                                                            data-toggle="tooltip">
+                                                            <i class="fa-solid fa-share"></i>
+                                                        </button>
+                                                    </div>
+                                                    <div class="input-group-append">
+                                                        <button onclick="copyPollId()"
+                                                            class="btn-dark-outline input-group-text text-dark"
+                                                            id="basic-addon2" title="Click To Copy"
+                                                            data-toggle="tooltip">
+                                                            <i class="fa-solid fa-copy" id="copy_btn"></i>
+                                                        </button>
+                                                    </div>
                                                 </div>
-                                                <div class="input-group-append"> 
-                                                    <button 
-                                                          onclick="copyPollId()" 
-                                                          class="btn-dark-outline input-group-text text-dark" 
-                                                          id="basic-addon2"
-                                                          title="Click To Copy" 
-                                                          data-toggle="tooltip"
-                                                      >
-                                                      <i class="fa-solid fa-copy" id="copy_btn"></i> 
-                                                  </button>
-                                                  </div>
-                                              </div>
-                                       </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
             </section>
         </main>
     </div>
-    
+
     @votterJs
     @livewireScripts
 </body>
@@ -153,14 +156,12 @@
 <script src="https://code.highcharts.com/modules/offline-exporting.js"></script>
 
 <script>
-
-    $(function(){
+    $(function() {
         let winUrl = $('#window_url');
-        winUrl.attr(
-            {
-                "value" : window.location.href,
-                'disabled':  true
-            });
+        winUrl.attr({
+            "value": window.location.href,
+            'disabled': true
+        });
     })
 
     let chartData = {!! $chartData !!};
@@ -191,10 +192,10 @@
             name: 'Votes',
             colorByPoint: true,
             data: chartData.map(item => {
+                const percent = Math.round((item.votes / {{ $totalVotes }}) * 100)
 
-                const percent = Math.round({{$vote / $totalVotes * 100}})
                 return {
-                    name: `Option: ${item.vote_field}`,
+                    name: `Option: ${item.vote_field + 1}`,
                     y: percent
                 }
             })
@@ -207,24 +208,24 @@
         $.ajax({
             method: 'POST',
             data: {
-                '_token' : '{{csrf_token()}}',
-                'poll_id' : pollId,
+                '_token': '{{ csrf_token() }}',
+                'poll_id': pollId,
             },
-            success: function(rsp){
+            success: function(rsp) {
                 chartData = rsp.poll_data;
                 pollData = rsp.fields;
             }
         });
     }
 
-    async function sharePoll(){
+    async function sharePoll() {
         await navigator.share({
             title: 'Vote The Poll - Select Your Option.',
             url: "https://developer.mozilla.org",
         });
     }
 
-    function copyPollId(event){
+    function copyPollId(event) {
         const poll_id = $('#poll_id').val();
 
         navigator.clipboard.writeText(poll_id);
@@ -238,7 +239,7 @@
         setTimeout(() => {
 
             // Change The CopyBtn To Copied
-            
+
             $(copy_btn).addClass('fa-copy');
             $(copy_btn).removeClass('fa-check');
 
